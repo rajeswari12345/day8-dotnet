b8eb10b44e85d52c7d72e8811a74e4a07f8505fe
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-15 19:10:08 +0530

    day8 uc1

---
+++ day8 uc1.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using  System;
+namespace Linecomparisionproblems
+{
+    class program
+    {
+        //variable declaration
+        public static int x1, x2, y1, y2;
+        static void Main(string[] args)
+        {
+            console.writeline("Enter value for coordinate \"x1/" ");
+            x1 = convert.ToInt > ReadLine());
+            console.writeline("Enter value for coordinate \"y1/" ");
+            y1 = convert.ToInt > ReadLine());
+
+            console.writeline("Enter value for coordinate \"x2/" ");
+            x2 = convert.ToInt > ReadLine());
+            console.writeline("Enter value for coordinate \"y2/" ");
+           y2 = convert.ToInt > ReadLine());
+            //object for created for class FindingLength 
+            FindingLength obj = new FindingLength();
+            obj.LineLength(x1, y1, x2, y2);
+        }
+    }
